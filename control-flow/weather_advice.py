ae10b5bde374608d9a4d4e8496dca8606610e74f
mogaya <mogayainnocent@gmail.com>
2024-09-21 14:13:07 +0300

    0. Weather Recommendation Program correction1

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,17 +1,17 @@
 # Ask the user to input the current weather from a predefined set of conditions
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
-# print(current_weather)
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
+# print(weather)
 
 # If the weather is “sunny”, recommend: Wear a t-shirt and sunglasses.
-if current_weather == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 
 # If the weather is “rainy”, recommend: Don't forget your umbrella and a raincoat.    
-elif current_weather == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
 
 # If the weather is “cold”, recommend: Make sure to wear a warm coat and a scarf.
-elif current_weather == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 
 # else statement that handles unexpected input by printing: Sorry, I don't have recommendations for this weather.
