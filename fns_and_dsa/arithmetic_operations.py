e35c4bc1f5956aaf73fd363a414219a3372b692b
mogaya <mogayainnocent@gmail.com>
2024-09-29 19:20:35 +0300

    Arithmetic Operations Function3

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,5 +10,5 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 print("You can not divide by zero")
-            else:
+            elif num2 != 0:
                 return num1/num2
