2c595f42244a3ab921491ebf7c6f6058116e3190
mogaya <mogayainnocent@gmail.com>
2024-10-13 19:40:24 +0300

    3. Distinguishing Between Class Methods and Static Methods

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,13 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        sum = a + b
+        return sum
+    
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        product = a * b
+        return product
